367c573efd51a3eafa69efe823574c2aa0d361f6
Souvik Mondal <souvikmondal1617@gmail.com>
2022-04-05 11:32:00 +0530

    Triangle and Square

---
+++ Potd/Triangle and Square.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+  public:
+    int countSquare(int b)
+    {
+	    int n=(b/2-1);
+	    return n*(n+1)/2;// code here 
+    }
+};
