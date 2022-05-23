4901c572337bbcc293c2419909001ccaa6bccbc4
Souvik Mondal <souvikmondal1617@gmail.com>
2022-05-23 17:20:17 +0530

    Course Schedule

---
+++ Potd/Course Schedule.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    public:
+    vector<int> findOrder(int n, int m, vector<vector<int>> pre) 
+   {
+       vector<vector<int>> adj(n);
+       vector<int> in(n,0);
+       for(auto i:pre)
+       {
+       adj[i[1]].push_back(i[0]);
+       in[i[0]]++;
+       }
+       queue<int> q;
+       vector<int> ans;
+       for(int i=0;i<n; i++)
+       {
+           if(in[i] == 0)
+           q.push(i);
+       }
+       int count =0;
+       while(!q.empty())
+       {
+           int node = q.front();
+           q.pop();
+           ans.push_back(node);
+           count++;
+           for(auto it:adj[node])
+           {
+               if(--in[it] == 0)
+               q.push(it);
+           }
+       }
+       if(n != count)
+       return {};
+       return ans;
+       
+   }
+};
