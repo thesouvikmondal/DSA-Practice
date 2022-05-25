8464fc48b919171f343a6fee2b6e7e4389763465
Souvik Mondal <souvikmondal1617@gmail.com>
2022-05-25 22:40:47 +0530

    push_new_node_in_middle_of_a_linked_list

--- Practice_codes/push_new_node_in_a_linked_list.cpp
+++ Practice_codes/push_new_node_in_a_linked_list.cpp
@@ -46,24 +46,24 @@ void pushAfter(node *previous_node , int data){
 
 }
 
-//add a new node in the last of a linked list
+// //add a new node in the last of a linked list
 
-void pushEnd(node **head_ref, int data){
-    node *newNode=new node();
-    newNode->data=data;
-    newNode->next=NULL;
-    node *last=*head_ref;
-    if(*head_ref==NULL){
-        *head_ref=newNode;
-        return;
-    }
-    while(last->next!=NULL){
-        last=last->next;
-    }
+// void pushEnd(node **head_ref, int data){
+//     node *newNode=new node();
+//     newNode->data=data;
+//     newNode->next=NULL;
+//     node *last=*head_ref;
+//     if(*head_ref==NULL){
+//         *head_ref=newNode;
+//         return;
+//     }
+//     while(last->next!=NULL){
+//         last=last->next;
+//     }
 
-    last->next=newNode;
-    return;
-}
+//     last->next=newNode;
+//     return;
+// }
 
 
 int main(){
@@ -75,7 +75,7 @@ int main(){
     
     pushAfter(head->next->next,65);
 
-    pushEnd(&head,77);
+   // pushEnd(&head,77);
 
 
     printList(head);
