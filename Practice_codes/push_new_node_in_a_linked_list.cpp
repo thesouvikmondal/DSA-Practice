545cd81aae4d18c214f8373d90a11d19ceaecb2c
Souvik Mondal <souvikmondal1617@gmail.com>
2022-05-25 22:40:03 +0530

    push_new_node_in_front_of_a_linked_list

--- Practice_codes/push_new_node_in_a_linked_list.cpp
+++ Practice_codes/push_new_node_in_a_linked_list.cpp
@@ -26,6 +26,7 @@ void printList (node *n){
     }
 }
 
+//push a  node in front of a linked list
 
 void push(node **head_ref , int data){
     node *newNode=new node();
@@ -35,11 +36,48 @@ void push(node **head_ref , int data){
 
 }
 
+//add a new node in middle of a linked list
+
+void pushAfter(node *previous_node , int data){
+    node *newNode = new node();
+    newNode->data=data;
+    newNode->next= previous_node->next;
+    previous_node->next= newNode;
+
+}
+
+//add a new node in the last of a linked list
+
+void pushEnd(node **head_ref, int data){
+    node *newNode=new node();
+    newNode->data=data;
+    newNode->next=NULL;
+    node *last=*head_ref;
+    if(*head_ref==NULL){
+        *head_ref=newNode;
+        return;
+    }
+    while(last->next!=NULL){
+        last=last->next;
+    }
+
+    last->next=newNode;
+    return;
+}
+
+
 int main(){
      node *head = NULL;
     push(&head , 5);
     push(&head , 7);
     push(&head , -3);
+    push(&head , 8);
     
+    pushAfter(head->next->next,65);
+
+    pushEnd(&head,77);
+
+
     printList(head);
 }
+
