d1b7ced4d0d6a7e56674cba9be33397ce626f0df
Souvik Mondal <souvikmondal1617@gmail.com>
2022-06-03 20:13:11 +0530

    using divide from the middle and reverse it

--- Linked_List/234. Palindrome Linked Lis.cpp
+++ Linked_List/234. Palindrome Linked Lis.cpp
@@ -18,27 +18,82 @@ using std::max;
   };
  
 class Solution {
+//using divide the linklist from middle and reverse the 2nd part
 public:
-
-    bool isPalindrome(vector<int>a){
-        int s=0,e=a.size()-1;
-        if(a.size()<=1){
-            return true;
+    ListNode* mid (ListNode * head){
+        ListNode* slw=head;
+        ListNode* fst=head->next;
+        while(fst && fst->next){
+            fst=fst->next->next;
+            slw=slw->next;
         }
-        while(s<e){
-            if(a[s]!=a[e]){
+        return slw;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        ListNode* cur=head;
+        ListNode* pre=NULL;
+        ListNode* nxt=NULL;
+        while(cur){
+            nxt=cur->next;
+            cur->next=pre;
+            pre=cur;
+            cur=nxt;
+        }
+        return pre;
+    }
+   
+    bool isPalindrome(ListNode* head) {
+        if(head->next==NULL) return true;
+        
+        ListNode* middle=mid(head);
+        
+        ListNode* temp=middle->next;
+        
+        middle->next=reverse(temp);
+        
+        ListNode* head1=head;
+        ListNode* head2=middle->next;
+        
+        while(head2){
+            if(head1->val!=head2->val){
                 return false;
             }
-        s++;e--;
+            head1=head1->next;
+            head2=head2->next;
         }
-   return true;     
-    }
-    bool isPalindrome(ListNode* head) {
-        vector<int> vec;
-        while(head){
-            vec.push_back(head->val);
-            head=head->next;
-        }
-        return isPalindrome(vec);
+        
+        temp=middle->next;
+        
+        middle->next=reverse(temp);
+        
+        return true;
+        
     }
 };
+
+//     // using push in array 
+// public:
+
+//     bool isPalindrome(vector<int>a){
+//         int s=0,e=a.size()-1;
+//         if(a.size()<=1){
+//             return true;
+//         }
+//         while(s<e){
+//             if(a[s]!=a[e]){
+//                 return false;
+//             }
+//         s++;e--;
+//         }
+//    return true;     
+//     }
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> vec;
+//         while(head){
+//             vec.push_back(head->val);
+//             head=head->next;
+//         }
+//         return isPalindrome(vec);
+//     }
+// };
