235edf145694fbf102b9f73a783a371f169489c1
Souvik Mondal <souvikmondal1617@gmail.com>
2022-06-11 11:20:19 +0530

    2235. Add Two Integers

---
+++ Array_String/2235. Add Two Integers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int sum(int num1, int num2) {
+        int ans=num1+num2;
+        return ans;
+    }
+};
