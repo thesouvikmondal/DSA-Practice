7383b283609893155a3ba30d10265aae5a1216e8
Souvik Mondal <souvikmondal1617@gmail.com>
2022-06-17 11:55:01 +0530

    solved using binary search

--- Array_String/852. Peak Index in a Mountain Array.cpp
+++ Array_String/852. Peak Index in a Mountain Array.cpp
@@ -1,4 +1,4 @@
-//using one triverse
+
 #include<bits/stdc++.h>
 #include<vector>
 #include <iostream>
@@ -9,19 +9,39 @@ using std::string;
 using std::max;
 using namespace std;
 class Solution {
-    public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size();
-        int p=INT_MIN;
-        for(int i=0;i<n;i++){
-                p=max(p,arr[i]);
+//using one triverse
+
+//     public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int n=arr.size();
+//         int p=INT_MIN;
+//         for(int i=0;i<n;i++){
+//                 p=max(p,arr[i]);
             
-        }
-        for(int i=0;i<n-1;i++){
-            if(p==arr[i]){
-                return i;
+//         }
+//         for(int i=0;i<n-1;i++){
+//             if(p==arr[i]){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//using binary search
+
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s=0,e=arr.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
             }
         }
-        return -1;
+        return s;
     }
 };
