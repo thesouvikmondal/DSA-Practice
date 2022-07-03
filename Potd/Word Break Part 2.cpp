394f23952a9d1bdda3ed6d241df006512fcc1dc4
Souvik Mondal <souvikmondal1617@gmail.com>
2022-07-03 22:58:47 +0530

    Word Break - Part 2

---
+++ Potd/Word Break Part 2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution{
+    public:
+   void solve(string s, string ans, unordered_set<string> &st, vector<string>& dict) {
+        if(s.length() <= 0) {
+            ans.pop_back();
+            dict.push_back(ans);
+        }
+    
+        for(int i = 0; i < s.length(); i++) {
+            string left = s.substr(0,i+1);
+    
+            if(st.find(left) != st.end()) {
+                solve(s.substr(i+1), ans + left + " ", st,dict);
+            }
+        }
+    }
+    
+    vector<string> wordBreak(int n, vector<string>& dict, string s)
+    {
+        unordered_set<string> st;
+        for(string str : dict)
+            st.insert(str);
+        
+        dict.clear();
+        solve(s,"",st,dict);
+        
+        return dict;
+    }
+};
