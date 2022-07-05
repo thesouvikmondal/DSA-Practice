fab6eaa886de09f29e497782915367c8455678f6
Souvik Mondal <souvikmondal1617@gmail.com>
2022-07-05 19:20:31 +0530

    Count BST nodes that lie in a given range

---
+++ Potd/Count BST nodes that lie in a given range.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+class Solution{
+    public:
+    void count(Node* root, int l,int h,int &cnt){
+        if(!root)
+            return;
+        
+        if(root->data >= l && root->data <= h){
+            cnt++;
+            count(root->left,l,h,cnt);
+            count(root->right,l,h,cnt);
+        }
+        else if(root->data < l){
+            count(root->right,l,h,cnt);
+        }
+        else if(root->data > h)
+            count(root->left,l,h,cnt);
+            
+    }
+    int getCount(Node *root, int l, int h)
+    {
+        if(!root)
+            return 0;
+        int cnt = 0;
+        count(root,l,h,cnt);
+        
+        return cnt;
+    }
+};
