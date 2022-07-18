9a071d95a5b6ccf38dcc3618b173322542475463
Souvik Mondal <souvikmondal1617@gmail.com>
2022-07-18 12:19:00 +0530

    Ceil in BST

---
+++ Potd/Ceil in BST.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int dfs(Node* root,int target){
+    if(!root) return INT_MAX;
+    int left = dfs(root->left, target);
+    int right = dfs(root->right, target);
+    return root->data >= target ? min({root->data,left,right}) : min(left,right);
+}
+int findCeil(Node* root, int input) {
+    if (root == NULL) return -1;
+    return dfs(root,input);
+}
