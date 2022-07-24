fb9056f7bb65cf88bc9f5b95becde0cd72d6b105
Souvik Mondal <souvikmondal1617@gmail.com>
2022-07-24 11:01:53 +0530

    Maximum sum leaf to root path

---
+++ Potd/Maximum sum leaf to root path.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+
+    struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+     public:
+    int maxPathSum(Node* r)
+    {   if(!r) return 0;
+        return r->data + max(maxPathSum(r->left),maxPathSum(r->right));
+    }};
