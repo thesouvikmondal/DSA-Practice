19cc623a0c85043ef16c98449a938e091e55c646
Souvik Mondal <souvikmondal1617@gmail.com>
2022-07-29 11:45:10 +0530

    189. Rotate Array

---
+++ Array_String/189. Rotate Array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+        void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
