3aa7ab01059a32979812f61e5aae2beac15e4fd6
Souvik Mondal <souvikmondal1617@gmail.com>
2022-08-03 10:48:46 +0530

    Preorder to BST

---
+++ Potd/Preorder to BST.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+
+{
+    int data;
+    struct Node *left, *right;
+} 
+
+
+{
+    Node* temp = (Node *)malloc( sizeof( Node ) );
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+
+class Solution{
+public:
+    //Function that constructs BST from its preorder traversal.
+   Node* post_order(int pre[], int size)
+    {
+        int idx = 0;
+        Node *root = buildTree(pre , idx , size  );
+        return root;
+    }
+    
+    Node *buildTree( int pre[] , int &idx  , int size ,int Min = -1e8 ,  int Max = 1e8 ){
+        
+        if(idx >= size )return NULL;
+        
+        if(pre[idx] < Min || pre[idx] > Max) return NULL;
+        
+        Node *root = newNode(pre[idx]);
+        idx++;
+        
+        root->left = buildTree(pre , idx , size , Min , root->data - 1);
+        
+        root->right = buildTree(pre , idx , size , root->data + 1 , Max );
+        
+        return root;
+    }
+};
