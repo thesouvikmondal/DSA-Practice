53b45f169f03c553e8a6210bb309e25a14013260
Souvik Mondal <souvikmondal1617@gmail.com>
2022-08-04 12:24:42 +0530

    reverse a string using for loop

---
+++ interviewQstn/reverse_a_string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//using for loop
+void reverseStr(string& str)
+{
+    int n = str.length();
+ 
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - 1 - i]);
+}
+int main(){
+   string str = "gfp";
+    reverseStr(str);
+    cout << str;
+    return 0;
+}
