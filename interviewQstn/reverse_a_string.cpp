f646d01b75367859ea0f1149fe098995bae19530
Souvik Mondal <souvikmondal1617@gmail.com>
2022-08-04 14:29:00 +0530

    reverse a string using while loop

--- interviewQstn/reverse_a_string.cpp
+++ interviewQstn/reverse_a_string.cpp
@@ -1,14 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//using for loop
+// //using for loop
+// void reverseStr(string& str)
+// {
+//     int n = str.length();
+ 
+//     for (int i = 0; i < n / 2; i++)
+//         swap(str[i], str[n - 1 - i]);
+// }
+
+
+//using while loop
 void reverseStr(string& str)
 {
-    int n = str.length();
+    int n = str.length()-1;
+    int i=0;
  
-    for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - 1 - i]);
+    while(i<=n){
+        swap(str[i], str[n]);
+        i++;n--;
+    }
 }
+
+
 int main(){
    string str = "gfp";
     reverseStr(str);
