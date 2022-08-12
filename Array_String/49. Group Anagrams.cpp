8f160e00fcc4e025f361ac48cbfa9d97c4952ee7
Souvik Mondal <souvikmondal1617@gmail.com>
2022-08-12 10:46:07 +0530

    49. Group Anagrams

---
+++ Array_String/49. Group Anagrams.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using std::vector;
+using std::string;
+using std::max;
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string >> umap;
+        for(auto x:strs){
+            string temp=x;
+             sort(x.begin(),x.end());
+             umap[x].push_back(temp);
+        }
+       
+        for(auto x:umap){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
