bdbe0850b3e070144af1a3a5f07478aac4573cc8
Souvik Mondal <souvikmondal1617@gmail.com>
2022-08-21 11:26:32 +0530

    Game with nos

---
+++ Potd/Game with nos.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+using std::vector;
+using std::string;
+using std::max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    int* game_with_number(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+   {
+       if(i!=n-1)
+       {
+        arr[i] = arr[i]^arr[i+1];    
+       }
+       
+   }
+   return arr;
+    // Complete the function
+    
+}
