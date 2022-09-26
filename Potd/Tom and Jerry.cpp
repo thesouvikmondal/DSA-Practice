a5ab3ea7a1184873320f26af2ebccb88f14e4226
Souvik Mondal <souvikmondal1617@gmail.com>
2022-09-26 22:29:22 +0530

    Tom and Jerry

---
+++ Potd/Tom and Jerry.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+int numsGame(int N) {
+
+        // code here
+
+        if(N%2 ==0){
+
+            return 1;
+
+        }
+
+        else{
+
+            return 0;
+
+        }
+
+    }
+};
