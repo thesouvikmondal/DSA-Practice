21b02ff1a9603ec400998ab59d5362fda9930e7e
Souvik Mondal <souvikmondal1617@gmail.com>
2022-10-17 18:49:02 +0530

    Replace every element with the least greater element on its right

---
+++ Potd/Replace every element with the least greater element on its right.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+vector<int> findLeastGreater(vector<int>& arr, int n) {
+
+        vector<int> v; 
+
+        set<int>s;
+
+        for(int i=n-1;i>=0;i--)
+
+        {
+
+            auto up=s.upper_bound(arr[i]);          //finds the nearest greater value
+
+            if(up==s.end())                         //if not find nearest  grater value
+
+            v.push_back(-1); 
+
+            else
+
+            v.push_back(*up);
+
+            
+
+            s.insert(arr[i]);                       //insert ans in array
+
+        }
+
+        reverse(v.begin(),v.end());            //reverse it cause we itarate to last
+
+        return v;
+
+        
+
+    }
+};
