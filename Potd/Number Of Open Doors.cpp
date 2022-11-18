4ef832d6b4863a6d7d0e97a43316687b3159e34a
Souvik Mondal <souvikmondal1617@gmail.com>
2022-11-18 20:42:31 +0530

    Number Of Open Doors

---
+++ Potd/Number Of Open Doors.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+int noOfOpenDoors(long long N) {
+        return sqrt(N); 
+    }
+};
