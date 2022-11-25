89679ad5bb6e2ec53b7e27bd085763c0369dbb2e
Souvik Mondal <souvikmondal1617@gmail.com>
2022-11-25 22:32:24 +0530

    Shreyansh and his bits

---
+++ Potd/Shreyansh and his bits.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+//java
+// long ncr(int n,int r){
+//         long ans=1;
+//         for(int i=1;i<=r;i++){
+//             ans=ans*(n-r+i)/i;
+//         }
+//         return ans;
+//     }
+    
+//     long count(long n)
+//     {
+//         // Code Here
+        
+//         String s=Long.toBinaryString(n);
+//         int c=0;
+//         long ans=0;
+//         int ind=0;
+//         for(int i=s.length()-1;i>=0;i--){
+//             ind++;
+//             if(s.charAt(i)=='1')
+//                {
+//                    c++;
+//                    if(ind!=c){
+//                        ans+=ncr(ind-1,c);
+//                    }
+//                }
+//         }
+//         return ans;
+//     }
+// }
+};
