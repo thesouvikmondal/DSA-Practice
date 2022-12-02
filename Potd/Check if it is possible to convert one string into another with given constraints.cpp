a4b9dc12f10bd82cee30d77691e98c3c4946d01d
Souvik Mondal <souvikmondal1617@gmail.com>
2022-12-02 11:28:39 +0530

    Check if it is possible to convert one string into another with given constraints

---
+++ Potd/Check if it is possible to convert one string into another with given constraints.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+//  int isItPossible(String S, String T, int M, int N) {
+//         // code here
+//         int i=0, j=0;
+        
+//         if(M!=N) return 0;
+        
+//         while(i<M || j<N){
+            
+//             while(i<M && S.charAt(i)=='#') i++;
+//             while(j<N && T.charAt(j)=='#') j++;
+            
+//             if(i>=M && j<N) return 0;
+//             if(i<M && j>=N) return 0;
+            
+//             if(i<M && j<N && S.charAt(i)!=T.charAt(j)) return 0;
+//             else{
+//                 if(i<M && S.charAt(i)=='A' && i<j) return 0;
+//                 else if(i<M && S.charAt(i)=='B' && i>j) return 0;
+//             }
+            
+//             i++;
+//             j++;
+//         }
+        
+//         return 1;
+//     }
+};
