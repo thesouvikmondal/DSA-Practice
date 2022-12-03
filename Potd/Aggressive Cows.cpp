2a6d9767714013348343e06839cb530068068206
Souvik Mondal <souvikmondal1617@gmail.com>
2022-12-03 10:27:59 +0530

    Aggressive Cows

---
+++ Potd/Aggressive Cows.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+// public static int solve(int n, int k, int[] stalls) {
+
+//         Arrays.sort(stalls);
+
+//         int l=0,r=stalls[n-1]-stalls[0];
+
+//         int ans=0;
+
+//         while(l<=r){
+
+//             int mid=(l+r)/2;
+
+//             int cow=1;
+
+//             int last=stalls[0];
+
+//             for(int i=1;i<n;i++){
+
+//                 if(stalls[i]-last>=mid){
+
+//                     cow++;
+
+//                     last=stalls[i];
+
+//                 }
+
+//             }
+
+//             if(cow>=k){
+
+//                 ans=mid;
+
+//                 l=mid+1;
+
+//             }else{
+
+//                 r=mid-1;
+
+//             }
+
+//         }
+
+//         return ans;
+
+//     }
+
+ 
+
+// }
+};
