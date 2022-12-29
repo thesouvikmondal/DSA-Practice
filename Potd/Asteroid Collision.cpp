e26f74927642aeba21d399ac6f9022b72a061a3a
Souvik Mondal <souvikmondal1617@gmail.com>
2022-12-29 21:40:40 +0530

    Asteroid Collision

---
+++ Potd/Asteroid Collision.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iterator>
+#include <set>
+using std::vector;
+using std::max;
+using namespace std;
+class Solution {
+public:
+vector<int> asteroidCollision(int N, vector<int> &ast) {
+        // code here
+        stack<int> st;
+        for(auto x:ast){
+            if(st.empty()){
+                st.push(x);
+            }
+            else{
+               if(x>0){
+                   st.push(x);
+               }
+               else{
+                   while(!st.empty() && st.top()>0 && st.top()<abs(x)){
+                       st.pop();
+                   }
+                   if(!st.empty() && st.top() == abs(x)){
+                       st.pop();
+                       continue;
+                   }
+                   if(!st.empty() && st.top()>abs(x)){
+                       continue;
+                   }
+                   st.push(x);
+               }
+            }
+        }
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
